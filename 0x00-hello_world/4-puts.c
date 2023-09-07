95b8597b8de1e5ba2645f760376d9164c3ac7881
mandillahjr2 <mandillahjr2@github.com>
2023-09-07 02:30:09 -0700

    puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+/**
+ * main is the main function block
+ * return 0 
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return 0;
 }
